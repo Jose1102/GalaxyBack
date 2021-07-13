0e341ea927c506a39bbc85f1f5961fff5a0cd376
Jose1102 <jose.castroortega1102@gmail.com>
2021-07-12 21:55:07 -0500

    Add crossing

--- src/main/java/escuelaing/arsw/galaxy/appgalaxy/controller/GalaxyGameController.java
+++ src/main/java/escuelaing/arsw/galaxy/appgalaxy/controller/GalaxyGameController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import escuelaing.arsw.galaxy.appgalaxy.repository.IUsuarioRepo;
 import escuelaing.arsw.galaxy.appgalaxy.model.Usuario;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://back-proyecto.herokuapp.com/")
 @RestController
 @RequestMapping("/galaxy")
 public class GalaxyGameController {
