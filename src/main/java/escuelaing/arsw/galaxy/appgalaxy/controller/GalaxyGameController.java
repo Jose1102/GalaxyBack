44bcf54676dae9c8ada31d946913d1a969b580ce
Jose1102 <jose.castroortega1102@gmail.com>
2021-07-13 02:36:59 -0500

    Add request

--- src/main/java/escuelaing/arsw/galaxy/appgalaxy/controller/GalaxyGameController.java
+++ src/main/java/escuelaing/arsw/galaxy/appgalaxy/controller/GalaxyGameController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import escuelaing.arsw.galaxy.appgalaxy.repository.IUsuarioRepo;
 import escuelaing.arsw.galaxy.appgalaxy.model.Usuario;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
 @RequestMapping("/galaxy")
 public class GalaxyGameController {
